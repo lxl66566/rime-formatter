58e7715c3488b15a3ebce1c818cb76480899efc8
lxl66566 <lxl66566@gmail.com>
2024-11-25 13:59:22 +0800

    fix: sort not as expected in sort_by: weight
    Signed-off-by: lxl66566 <lxl66566@gmail.com>

--- src/dict_fmt.ts
+++ src/dict_fmt.ts
@@ -49,6 +49,9 @@ function dict_fmt(text: string, option: ConfigType): string {
   return result.join("\n");
 }
 
+/*
+ * 按照 spell 字典序排序。如果 spell 相同，则按 rank 降序。
+ */
 function compare_by_spell(self: Word, other: Word): number {
   if (self.spell < other.spell) return -1;
   if (self.spell > other.spell) return 1;
@@ -59,14 +62,18 @@ function compare_by_spell(self: Word, other: Word): number {
   return other.rank - self.rank;
 }
 
+/*
+ * 按照 rank 降序。如果 rank 相同，则按 spell 字典序排序。
+ */
 function compare_by_weight(self: Word, other: Word): number {
+  if (self.rank === null && other.rank === null) return self.spell < other.spell ? -1 : 1;
   if (self.rank === null) return 1;
   if (other.rank === null) return -1;
 
   if (self.rank < other.rank) return 1;
   if (self.rank > other.rank) return -1;
 
-  return other.spell.localeCompare(self.spell);
+  return self.spell < other.spell ? -1 : 1;
 }
 
 export default dict_fmt;
